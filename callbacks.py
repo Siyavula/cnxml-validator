aa17822dff34f133554d1354b700a1dc29ec0656
Carl Scheffler <carl@siyavula.com>
2012-10-22 11:47:37 +0200

    Implemented linked-concepts environment.

--- callbacks.py
+++ callbacks.py
@@ -141,3 +141,10 @@ def check_link_element(element):
     if targetPresent and (element.text != ''):
         raise_error("<link> with target-id must not contain text")
     return True
+
+def is_subject(element):
+    valid = ['maths', 'science']
+    subject = element.text.strip().lower()
+    if subject not in valid:
+        raise_error("linked-concepts/concept/subject must be one of %s, found '%s' instead"%(repr(valid), element.text))
+    return True
