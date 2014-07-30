636e195b9ba394572e0893240926b82df6ee3635
Ewald Zietsman <ewald@siyavula.com>
2014-07-30 16:42:39 +0200

    Major cleanup of validator repo. Tweaks to scripts to make them work with new folder structure

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='cnxml-validator',
+      version='1.1',
+      description='''Tools for validating and transforming Siyavula\'s authoring
+      formats''',
+      author='Siyavula Education',
+      author_email='info@siyavula.com',
+      url='http://www.siyavula.com/',
+      packages=['XmlValidator'],
+      scripts=['cnxmlplus2html', 'cnxmlplus2latex', 'cnxmlplus-validate'],
+      install_requires=['xml.sax.saxutils',
+                        'lxml',
+                        'argparse',
+                        'termcolor',
+                        'siyavula.transforms'],
+      )
