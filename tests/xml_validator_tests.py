0c57fdc47a6f9c95c5d97a808d9a0033fe4ca05d
Richard Cochrane <screamer42@gmail.com>
2016-04-07 17:46:31 +0200

    Fix note tag usage - can now willingly accept content

--- tests/xml_validator_tests.py
+++ tests/xml_validator_tests.py
@@ -1,6 +1,6 @@
 """
-This part of the test suite focuses on testing specific xml tags to ensure 
-they are performing as expected. This also tests that the validator does 
+This part of the test suite focuses on testing specific xml tags to ensure
+they are performing as expected. This also tests that the validator does
 in fact load a spec and validate against that spec.
 """
 
@@ -55,7 +55,7 @@ class ExerciseValidatorTests(TestCase):
 
     def test_validate_with_valid_xml(self):
         """
-        This should represent the minimum needed elements for a template. 
+        This should represent the minimum needed elements for a template.
         Response is actually optional since that is not needed for entries in books.
         """
         good_template_dom = etree.fromstring('''
@@ -95,6 +95,40 @@ class ExerciseValidatorTests(TestCase):
                 <solution>
                     <note type="note">
                     </note>
+                </solution>
+            </entry>
+        </exercise-container>''')
+
+        assert self.exercise_validator.validate(good_template_dom) is None
+
+    def test_validate_with_note_tag_with_text(self):
+        good_template_dom = etree.fromstring('''
+        <exercise-container>
+            <meta>
+            </meta>
+            <entry>
+                <problem>
+                    <note type="note">Note Content</note>
+                </problem>
+                <solution>
+                    <note type="tip">Note Content</note>
+                </solution>
+            </entry>
+        </exercise-container>''')
+
+        assert self.exercise_validator.validate(good_template_dom) is None
+
+    def test_validate_with_note_tag_with_para(self):
+        good_template_dom = etree.fromstring('''
+        <exercise-container>
+            <meta>
+            </meta>
+            <entry>
+                <problem>
+                    <note type="note"><para>Note Content</para></note>
+                </problem>
+                <solution>
+                    <note type="tip"><para>Note Content</para></note>
                 </solution>
             </entry>
         </exercise-container>''')
@@ -179,8 +213,8 @@ class ExerciseValidatorTests(TestCase):
     #@raises(XmlValidationError)
     def test_validate_with_nuclear_notation_tag_no_symbol(self):
         """
-        This should raise an error since the symbol tag should be required. 
-        The problem lies in the unordered modifier since the spec for that 
+        This should raise an error since the symbol tag should be required.
+        The problem lies in the unordered modifier since the spec for that
         is a hack and matches incorrect patterns. This needs to be corrected.
         """
         good_template_dom = etree.fromstring('''
@@ -203,9 +237,9 @@ class ExerciseValidatorTests(TestCase):
 
     def test_validate_with_nuclear_notation_tag_no_children(self):
         """
-        This should raise an error since nuclear_notation is required to contain at 
-        least the symbol tag. The problem lies in the unordered modifier 
-        since the spec for that is a hack and matches incorrect patterns. 
+        This should raise an error since nuclear_notation is required to contain at
+        least the symbol tag. The problem lies in the unordered modifier
+        since the spec for that is a hack and matches incorrect patterns.
         This needs to be corrected.
         """
         good_template_dom = etree.fromstring('''
@@ -226,7 +260,7 @@ class ExerciseValidatorTests(TestCase):
     def test_validate_with_currency_tag_no_children(self):
         """
         This should raise an error since currency is required to contain at least the number tag.
-        The problem lies in the unordered modifier since the spec for that is a 
+        The problem lies in the unordered modifier since the spec for that is a
         hack and matches incorrect patterns. This needs to be corrected.
         """
         good_template_dom = etree.fromstring('''
@@ -246,9 +280,9 @@ class ExerciseValidatorTests(TestCase):
 
     def test_validate_with_pspicture_tag_no_children(self):
         """
-        This should raise an error since currency is required to contain at 
+        This should raise an error since currency is required to contain at
         least either src or code child.
-        The problem lies in the unordered modifier since the spec 
+        The problem lies in the unordered modifier since the spec
         for that is a hack and matches incorrect patterns.
         This needs to be corrected.
         """
@@ -269,9 +303,9 @@ class ExerciseValidatorTests(TestCase):
 
     def test_validate_with_tikzpicture_tag_no_children(self):
         """
-        This should raise an error since tikzpicture is required to contain 
+        This should raise an error since tikzpicture is required to contain
         at least either src or code child.
-        The problem lies in the unordered modifier since the spec for that 
+        The problem lies in the unordered modifier since the spec for that
         is a hack and matches incorrect patterns. This needs to be corrected.
         """
         good_template_dom = etree.fromstring('''
@@ -312,7 +346,7 @@ class ExerciseValidatorTests(TestCase):
     #@raises(XmlValidationError)
     def test_validate_with_number_tag(self):
         """
-        Testing that the number tag works and checks the type of number. 
+        Testing that the number tag works and checks the type of number.
         This really should fail, oh dear another bad instance.
         """
         good_template_dom = etree.fromstring('''
@@ -337,7 +371,7 @@ class ExerciseValidatorTests(TestCase):
         Title is not optional but the validator does not actually check that it is present.
         Difficulty is also not optional. This must contain level in the updated validator.
         Language is not optional. Only valid language codes are accepted here: en, en-ZA, af, af-ZA.
-        Link is optional and there can be multiple links. 
+        Link is optional and there can be multiple links.
         Link must be self-closing and contain rel and href as attributes
         """
         good_template_dom = etree.fromstring('''
